30f0d12b0561d0d3a95d1f487b9e889912d5d32d
Leon D. Pena L <leonplondon@gmail.com>
2017-09-29 11:21:23 -0500

    Exported createPool function to enable pool connections

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import { createConnection } from 'mysql2/promise';
+import { createConnection, createPool } from 'mysql2/promise';
 import Model from './Model';
 
 export {
   Model,
   createConnection,
+  createPool,
 };
